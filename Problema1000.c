1df310b868690d8d24ad9d5d0ef43b674f894d5d
Yortim21 <nunesfilipenobre@gmail.com>
2024-10-19 14:39:35 -0300

    Mais questoes

--- Problema1000.c
+++ Problema1000.c
@@ -1,6 +1,11 @@
-#include <stdio.h>
+#include <stdio.h> /*Sempre incluir standard input e output para receber e printar informacoes*/
 
+/*Funcao main executa automaticamente o codigo principal*/
+/*Nao e necessario retornar algum valor para a main mesmo que a funcao seja int*/
+/*CUIDADO, o Beecrowd nao reconhece void main()*/
 int main()
 {
-    printf("Hello World!\n");
+    printf("Hello World!\n"); /*Printa a frase "Hello World!"*/
+    /*O "\n" no final faz o texto pular para a proxima linha*/
+    /*CUIDADO Todos os exercicios do Beecrowd demandam o "\n" no final*/
 }
