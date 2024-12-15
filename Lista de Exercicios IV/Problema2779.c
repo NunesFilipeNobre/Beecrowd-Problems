09566b501e33b26d87380d2b93fb17fad40a777f
Filipe Nunes <nunesfilipenobre@gmail.com>
2024-12-15 19:49:30 -0300

    Mais questoes

--- Lista de Exercicios IV/Problema2779.c
+++ Lista de Exercicios IV/Problema2779.c
@@ -2,5 +2,27 @@
 
 int main()
 {
-    
+    int totalFig, compradas, restante, tempFig=0;
+    scanf("%d",&totalFig);
+    int fig[totalFig];
+    scanf("%d",&compradas);
+
+    for (int i = 0; i < totalFig; i++)
+    {
+        fig[i]=0;
+    }
+    for (int i = 0; i < compradas; i++)
+    {
+        scanf("%d",&tempFig);
+        fig[tempFig-1]+=1;
+    }
+    restante=totalFig;
+    for (int i = 0; i < totalFig; i++)
+    {
+        if (fig[i]>0)
+        {
+            restante-=1;
+        }
+    }
+    printf("%d\n",restante);
 }
--- Lista de Exercicios IV/Problema2782.c
+++ Lista de Exercicios IV/Problema2782.c
@@ -2,5 +2,25 @@
 
 int main()
 {
-    
+    int tamanho,escadinha=1; /*tamanho eh a quantidade de numeros, escadinha o total de escadinhas*/
+    scanf("%d",&tamanho); /*Recebe o tamanho*/
+    int num[tamanho], diferencas[tamanho];  /*Vetor num guarda cada numero na sequencia*/
+                                            /*Diferencas guarda a diferenca entre um numero e seu sucessor*/
+    for (int i = 0; i < tamanho; i++) /*Loop que passa por todos os numeros*/
+    {
+        scanf("%d",&num[i]); /*Recebe o valor de todos os numeros*/
+    }
+
+    for (int i = 0; i < (tamanho-1); i++) /*Loop repete (tamanho-1) vezes pois sao (n-1) diferencas entre n numeros*/
+    {
+        diferencas[i]=num[i]-num[i+1]; /*Pega as diferencas entre cada numero*/
+    }
+    for (int i = 0; i < (tamanho-2); i++) /*Loop repete (tamanho-2) vezes pos eh a diferenca entre duas diferencas, que por sua vez sao (tamanho-1)*/
+    {
+         if (diferencas[i]-diferencas[i+1]) /*Verifica se as diferenca entre 2 diferencas sao iguais*/
+        {
+            escadinha+=1; /*Se sim, existe uma mudanca na regra da sequencia, portanto, +1 para o numero de escadinhas, ja que entramos na proxima sequencia*/
+        }
+    }
+    printf("%d\n",escadinha); /*Printa a quantidade de escadinhas*/
 }
