3338c3c0ed065cb41c363f67ee3b8245f0c2cd62
Filipe Nunes <nunesfilipenobre@gmail.com>
2024-12-15 21:49:48 -0300

    Fibonatasso

--- Lista de Exercicios IV/Problema1176.c
+++ Lista de Exercicios IV/Problema1176.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
 
+unsigned long long int fibonacci(int n) /*Funcao fibonacci pra ficar bonitinho*/
+{
+    unsigned long long int sequencia[61]; /*Vetor de 61 numeros, de 0 a 60*/
+        for (int i = 0; i <= n; i++) /*Loop que repete ate o numero desejado*/
+        {
+            if (i==0 || i==1) /*Excecoes para 0 e 1*/
+            {
+                sequencia[i]=i;
+            }
+            else
+            {
+                sequencia[i]=sequencia[i-1]+sequencia[i-2]; /*Regra de fibonacci*/
+            }
+        }
+        return sequencia[n]; /*Retorna o valor do nesimo numero da sequencia*/
+}
+
 int main()
 {
-    
+    int casos, num; /*Variaveis de numero de casos e numero da sequencia de fibonacci*/
+    unsigned long long int result; /*unsigned long long int tem 64bits pedidos pela questao*/
+    scanf("%d",&casos); /*Recebe quantos casos serao*/
+    for (int i = 0; i < casos; i++)
+    {
+        scanf("%d",&num); /*Recebe o numero desejado da sequencia*/
+        result=fibonacci(num); /*Aplica o numero na sequencia de fibonacci*/
+        printf("Fib(%d) = %llu\n",num,result); /*Printa o valor do numero na sequencia de fibonacci*/
+    }   
 }
--- Lista de Exercicios IV/Problema2783.c
+++ Lista de Exercicios IV/Problema2783.c
@@ -2,5 +2,33 @@
 
 int main()
 {
-    
+    int totalFig, compradas, restante, tempFig=0, totalCarimb;
+    scanf("%d",&totalFig);
+    scanf("%d",&totalCarimb);
+    int fig[totalFig], carimbadas[totalCarimb];
+    scanf("%d",&compradas);
+
+    for (int i = 0; i < totalFig; i++)
+    {
+        fig[i]=0;
+    }
+    for (int i = 0; i < totalCarimb; i++)
+    {
+        scanf("%d",&carimbadas[i]);
+        carimbadas[i]-=1;
+    }
+    for (int i = 0; i < compradas; i++)
+    {
+        scanf("%d",&tempFig);
+        fig[tempFig-1]+=1;
+    }
+    restante=totalCarimb;
+    for (int i = 0; i < totalCarimb; i++)
+    {
+        if (fig[carimbadas[i]]>0)
+        {
+            restante-=1;
+        }
+    }
+    printf("%d\n",restante);
 }
